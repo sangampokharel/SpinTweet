d94d200b80fb28cbd1a84ce2b58e79331a16733d
sangam <sangampokharel803@gmail.com>
2024-02-18 20:17:41 +0545

    Initial Commit

---
+++ SpinTweet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpinTweet
+//
+//  Created by sangam pokharel on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpinTweet/SpinTweetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinTweetApp.swift
+//  SpinTweet
+//
+//  Created by sangam pokharel on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpinTweetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
